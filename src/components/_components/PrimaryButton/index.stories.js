2466b238afba1cb87446f18b8a371d4234d93f6c
Stanislav <stanislavja@yandex.ru>
2022-05-17 01:50:06 -0700

    add storybook

---
+++ src/components/_components/PrimaryButton/index.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Button from './index'
+
+export default {
+    title: 'Design System/Atoms/Button',
+    component: Button,
+};
+
+export const PrimaryButton = () => <Button>Button</Button>
