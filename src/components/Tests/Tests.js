d777c9a47cafe3f49a4ff2c4c629faa6c8f19357
Andrey Rumyantsev <avrumyantsev@avito.ru>
2022-06-21 11:32:07 +0300

    omit loader animation from test

--- src/components/Tests/Tests.js
+++ src/components/Tests/Tests.js
@@ -111,15 +111,17 @@ const Tests = () => {
                             alt="img"
                             className={cls.Img}
                           />
-                        ) : (
-                          <Loader size={'35px'} />
-                        )}
+                        ) 
+                        : (
+                          <Loader size={'0px'} />
+                        )
+                        }
                         <div className={cls.testQues}>
                           <div>
                             <h2 className={cls.count}>{++index}</h2>
                             <div className={cls.line} />
                             <p className={cls.question}>
-                              {title + '⠀'}
+                              {title} 
                               <span className={cls.attention}>
                                 {question_type === 'R'
                                   ? ' (выберите 1 вариант ответа)'
